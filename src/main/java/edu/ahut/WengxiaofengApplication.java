bac5a77a43b29786885c078b7dcdb2496f656862
wangwanqiang <18395585695@163.com>
2018-05-07 14:06:43 +0800

    jscommit

--- src/main/java/edu/ahut/WengxiaofengApplication.java
+++ src/main/java/edu/ahut/WengxiaofengApplication.java
@@ -1,12 +1,9 @@
 package edu.ahut;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
